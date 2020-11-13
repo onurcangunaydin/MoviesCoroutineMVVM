bdfd25c1d08f3868b00f4f49dc0ce8c36c28bb06
Onurcan <onurcangunaydin@gmail.com>
2020-11-13 15:44:43 +0300

    create Recylerview adapter

--- app/src/main/java/com/example/movies_coroutine_mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/movies_coroutine_mvvm/ui/MainActivity.kt
@@ -4,44 +4,42 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.movies_coroutine_mvvm.R
 import com.example.movies_coroutine_mvvm.repository.MoviesRepository
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(){
-
-
     private lateinit var viewModel: MovieViewModel
     private lateinit var adapter: MoviesAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val viewModelFactory = ViewModelFactory(MoviesRepository())
-
-        viewModel = ViewModelProvider(this,viewModelFactory).get(MovieViewModel::class.java)
-        setUI()
-
-        viewModel.movie?.observe(this, Observer {
-           val movieList =  it.data?.results?: emptyList()
-            adapter.updateDataSet(movieList)
-        })
-
-        viewModel.getMovies(1)
-
+        setViewModel()
+    }
+     //kullanicin gordugu ui baslatildigi yer.
+    override fun onStart() {
+        super.onStart()
+        setData()
+         setUI()
     }
 
   private fun setUI (){
-      rv_movie.layoutManager = LinearLayoutManager(this@MainActivity)
+      rv_movie.layoutManager = GridLayoutManager(this,2)
       adapter = MoviesAdapter()
       rv_movie.adapter = adapter
-
   }
-
-
-
+    private fun setData(){
+        viewModel.movie?.observe(this, Observer {
+            val movieList =  it.data?.results?: emptyList()
+            adapter.updateDataSet(movieList)
+        })
+    }
+    private fun setViewModel(){
+        val viewModelFactory = ViewModelFactory(MoviesRepository())
+        viewModel = ViewModelProvider(this,viewModelFactory).get(MovieViewModel::class.java)
+        viewModel.getMovies(1)
+    }
 }
 
--- app/src/main/java/com/example/movies_coroutine_mvvm/ui/MoviesAdapter.kt
+++ app/src/main/java/com/example/movies_coroutine_mvvm/ui/MoviesAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.example.movies_coroutine_mvvm.R
 import com.example.movies_coroutine_mvvm.data.model.Result
 
@@ -19,17 +20,12 @@ class MoviesAdapter(private val resultList: MutableList<Result> = mutableListOf(
 
         fun bind(result: Result){
             titleTextView.setText(result.title)
-
-//            val requestOptions = RequestOptions()
-//                    .placeholder(R.drawable.ic_launcher_background)
-//                    .error(R.drawable.ic_launcher_background)
-
             Glide.with(itemView.context)
-                    .load(result.posterPath)
+                .load("https://image.tmdb.org/t/p/w342${result.posterPath}")
+                .transform(CenterCrop())
                     .into(moviePoster)
         }
     }
-
      fun updateDataSet(newResultList: List<Result>){
          resultList.clear()
          resultList.addAll(newResultList)
@@ -39,27 +35,17 @@ class MoviesAdapter(private val resultList: MutableList<Result> = mutableListOf(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
         return MoviesViewHolder(
             LayoutInflater.from(parent.context).inflate(
-                R.layout.movie_item,
+                R.layout.item_movies,
                     parent,
                     false
             )
         )
     }
-
     override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
-        holder.bind(resultList.get(position))
-
+        holder.bind(resultList[position])
         }
-
 
     override fun getItemCount(): Int {
        return resultList.size
-    }
-
-
-    private var onItemClickListener: ((Result) -> Unit)? = null
-
-    fun setOnItemClicklistener(listener: (Result) -> Unit) {
-        onItemClickListener = listener
     }
 }
